cb858546b485507ae53b7326798e14ab5d32830a
Mehdi Moulati <mmoulati@e1r12p12.1337.ma>
2024-07-15 14:12:19 +0100

    add more tests for verification

--- mini-moul/tests/C07/ex04/ft_convert_base.c
+++ mini-moul/tests/C07/ex04/ft_convert_base.c
@@ -1,7 +1,7 @@
+#include "../../../../ex04/ft_convert_base.c"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../../../../ex04/ft_convert_base.c"
 #if __has_include("../../../../ex04/ft_convert_base2.c")
 # include "../../../../ex04/ft_convert_base2.c"
 #endif
@@ -9,89 +9,230 @@
 
 typedef struct s_test
 {
-    char *desc;
-    char *nbr;
-    char *base_from;
-    char *base_to;
-    char *expected;
-} t_test;
+	char	*desc;
+	char	*nbr;
+	char	*base_from;
+	char	*base_to;
+	char	*expected;
+}			t_test;
 
-int run_tests(t_test *tests, int count);
+int			run_tests(t_test *tests, int count);
 
-int main(void)
+int	main(void)
 {
-    t_test tests[] = {
-        {
-            .desc = "Convert positive decimal to binary",
-            .nbr = "42",
-            .base_from = "0123456789",
-            .base_to = "01",
-            .expected = "101010",
-        },
-        {
-            .desc = "Convert negative decimal to binary",
-            .nbr = "-42",
-            .base_from = "0123456789",
-            .base_to = "01",
-            .expected = "-101010",
-        },
-        {
-            .desc = "Convert binary to hexadecimal",
-            .nbr = "101010",
-            .base_from = "01",
-            .base_to = "0123456789ABCDEF",
-            .expected = "2A",
-        },
-        {
-            .desc = "Invalid base_from",
-            .nbr = "42",
-            .base_from = "01234567899",
-            .base_to = "01",
-            .expected = NULL,
-        },
-        {
-            .desc = "Invalid base_to",
-            .nbr = "42",
-            .base_from = "0123456789",
-            .base_to = "5",
-            .expected = NULL,
-        },
-    };
-    int count = sizeof(tests) / sizeof(tests[0]);
+	int	count;
 
-    return run_tests(tests, count);
+	t_test tests[] = {
+		{
+			.desc = "Convert positive decimal to binary",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "01",
+			.expected = "101010",
+		},
+		{
+			.desc = "Convert negative decimal to hexadecimal",
+			.nbr = "-255",
+			.base_from = "0123456789",
+			.base_to = "0123456789ABCDEF",
+			.expected = "-FF",
+		},
+		{
+			.desc = "Convert binary to decimal",
+			.nbr = "1010",
+			.base_from = "01",
+			.base_to = "0123456789",
+			.expected = "10",
+		},
+		{
+			.desc = "Convert hexadecimal to decimal",
+			.nbr = "FF",
+			.base_from = "0123456789ABCDEF",
+			.base_to = "0123456789",
+			.expected = "255",
+		},
+		{
+			.desc = "Convert with whitespace and sign",
+			.nbr = "   ---++--42",
+			.base_from = "0123456789",
+			.base_to = "0123456789ABCDEF",
+			.expected = "-2A",
+		},
+		{
+			.desc = "Convert zero",
+			.nbr = "0",
+			.base_from = "0123456789",
+			.base_to = "01",
+			.expected = "0",
+		},
+		{
+			.desc = "Convert INT_MIN",
+			.nbr = "-2147483648",
+			.base_from = "0123456789",
+			.base_to = "0123456789ABCDEF",
+			.expected = "-80000000",
+		},
+		{
+			.desc = "Convert INT_MAX",
+			.nbr = "2147483647",
+			.base_from = "0123456789",
+			.base_to = "0123456789ABCDEF",
+			.expected = "7FFFFFFF",
+		},
+		{
+			.desc = "Convert between same base",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "0123456789",
+			.expected = "42",
+		},
+		{
+			.desc = "Invalid character in number",
+			.nbr = "42X",
+			.base_from = "0123456789",
+			.base_to = "01",
+			.expected = NULL,
+		},
+		{
+			.desc = "Invalid base (duplicate character)",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "011",
+			.expected = NULL,
+		},
+		{
+			.desc = "Invalid base (contains '+')",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "01+",
+			.expected = NULL,
+		},
+		{
+			.desc = "Invalid base (contains '-')",
+			.nbr = "42",
+			.base_from = "01-23456789",
+			.base_to = "01",
+			.expected = NULL,
+		},
+		{
+			.desc = "Invalid base (contains space)",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "01 23456789",
+			.expected = NULL,
+		},
+		{
+			.desc = "Empty number string",
+			.nbr = "",
+			.base_from = "0123456789",
+			.base_to = "01",
+			.expected = NULL,
+		},
+		{
+			.desc = "Empty base_from",
+			.nbr = "42",
+			.base_from = "",
+			.base_to = "01",
+			.expected = NULL,
+		},
+		{
+			.desc = "Empty base_to",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "",
+			.expected = NULL,
+		},
+		{
+			.desc = "Base with length 1",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "0",
+			.expected = NULL,
+		},
+		{
+			.desc = "Convert to base 36",
+			.nbr = "123456789",
+			.base_from = "0123456789",
+			.base_to = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+			.expected = "21I3V9",
+		},
+		{
+			.desc = "Convert from base 36",
+			.nbr = "HELLO",
+			.base_from = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+			.base_to = "0123456789",
+			.expected = "29234652",
+		},
+		{
+			.desc = "Convert positive decimal to binary",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "01",
+			.expected = "101010",
+		},
+		{
+			.desc = "Convert negative decimal to binary",
+			.nbr = "-42",
+			.base_from = "0123456789",
+			.base_to = "01",
+			.expected = "-101010",
+		},
+		{
+			.desc = "Convert binary to hexadecimal",
+			.nbr = "101010",
+			.base_from = "01",
+			.base_to = "0123456789ABCDEF",
+			.expected = "2A",
+		},
+		{
+			.desc = "Invalid base_from",
+			.nbr = "42",
+			.base_from = "01234567899",
+			.base_to = "01",
+			.expected = NULL,
+		},
+		{
+			.desc = "Invalid base_to",
+			.nbr = "42",
+			.base_from = "0123456789",
+			.base_to = "5",
+			.expected = NULL,
+		},
+	};
+	count = sizeof(tests) / sizeof(tests[0]);
+	return (run_tests(tests, count));
 }
 
-int run_tests(t_test *tests, int count)
+int	run_tests(t_test *tests, int count)
 {
-    int i;
-    int error = 0;
+	int		i;
+	int		error;
+	char	*result;
 
-    for (i = 0; i < count; i++)
-    {
-        char *result = ft_convert_base(tests[i].nbr, tests[i].base_from, tests[i].base_to);
-
-        if (!result && !tests[i].expected)
-        {
-            printf(GREEN CHECKMARK GREY " [%d] %s\n" DEFAULT, i + 1, tests[i].desc);
-        }
-        else if (!result || !tests[i].expected)
-        {
-            printf(RED "[%d] %s got \"%s\" instead of \"%s\"\n" DEFAULT, i + 1, tests[i].desc, result, tests[i].expected);
-            error -= 1;
-        }
-        else if (strcmp(result, tests[i].expected) != 0)
-        {
-            printf(RED "[%d] %s got \"%s\" instead of \"%s\"\n" DEFAULT, i + 1, tests[i].desc, result, tests[i].expected);
-            error -= 1;
-        }
-        else
-        {
-            printf(GREEN CHECKMARK GREY " [%d] %s got \"%s\" as expected\n" DEFAULT, i + 1, tests[i].desc, result);
-        }
-
-        free(result);
-    }
-
-    return error;
+	error = 0;
+	for (i = 0; i < count; i++)
+	{
+		result = ft_convert_base(tests[i].nbr, tests[i].base_from,
+				tests[i].base_to);
+		if (!result && !tests[i].expected)
+		{
+			printf(GREEN CHECKMARK GREY " [%d] %s\n" DEFAULT, i + 1,	tests[i].desc);
+		}
+		else if (!result || !tests[i].expected)
+		{
+			printf(RED "[%d] %s got \"%s\" instead of \"%s\"\n" DEFAULT, i + 1,	tests[i].desc, result, tests[i].expected);
+			error -= 1;
+		}
+		else if (strcmp(result, tests[i].expected) != 0)
+		{
+			printf(RED "[%d] %s got \"%s\" instead of \"%s\"\n" DEFAULT, i + 1,	tests[i].desc, result, tests[i].expected);
+			error -= 1;
+		}
+		else
+		{
+			printf(GREEN CHECKMARK GREY " [%d]	%s got \"%s\" as expected\n" DEFAULT, i + 1, tests[i].desc,	result);
+		}
+		free(result);
+	}
+	return (error);
 }
--- mini-moul/tests/C07/ex05/ft_split.c
+++ mini-moul/tests/C07/ex05/ft_split.c
@@ -1,173 +1,258 @@
+#include "../../../../ex05/ft_split.c"
+#include "../../../utils/constants.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../../../../ex05/ft_split.c"
-#include "../../../utils/constants.h"
 
 typedef struct s_test
 {
-    char *desc;
-    char *str;
-    char *charset;
-    char **expected;
-} t_test;
+	char	*desc;
+	char	*str;
+	char	*charset;
+	char	**expected;
+}			t_test;
 
-int run_tests(t_test *tests, int count);
+int			run_tests(t_test *tests, int count);
 
-int main(void)
+int	main(void)
 {
-    t_test tests[] = {
-       {
-            .desc = "Empty string with empty charset",
-            .str = "",
-            .charset = "",
-            .expected = (char *[1]) {0},
-        },
-        {
-            .desc = "Single-word string with empty charset",
-            .str = "hello",
-            .charset = "",
-            .expected = (char *[2]) {"hello", 0},
-        },
-        {
-            .desc = "String with leading and trailing separators",
-            .str = ",,hello,world,,",
-            .charset = ",",
-            .expected = (char *[3]) {"hello", "world", 0},
-        },
-        {
-            .desc = "String with multiple consecutive separators",
-            .str = "hello,,,,world",
-            .charset = ",",
-            .expected = (char *[3]) {"hello", "world", 0},
-        },
-        {
-            .desc = "String with repeated separators",
-            .str = "aaabbbaaaccc",
-            .charset = "ab",
-            .expected = (char *[]){ "ccc", 0 },
-        },
-    };
-    int count = sizeof(tests) / sizeof(tests[0]);
+	int	count;
 
-    return run_tests(tests, count);
+	t_test tests[] = {
+		{
+			.desc = "Basic split with single character separator",
+			.str = "hello,world,how,are,you",
+			.charset = ",",
+			.expected = (char *[]){"hello", "world", "how", "are", "you", NULL},
+		},
+		{
+			.desc = "Split with multiple character separator",
+			.str = "apple##banana##cherry##date",
+			.charset = "##",
+			.expected = (char *[]){"apple", "banana", "cherry", "date", NULL},
+		},
+		{
+			.desc = "Split with multiple different separators",
+			.str = "one,two;three:four",
+			.charset = ",;:",
+			.expected = (char *[]){"one", "two", "three", "four", NULL},
+		},
+		{
+			.desc = "Empty string",
+			.str = "",
+			.charset = ",",
+			.expected = (char *[]){NULL},
+		},
+		{
+			.desc = "No separators in string",
+			.str = "hello",
+			.charset = ",",
+			.expected = (char *[]){"hello", NULL},
+		},
+		{
+			.desc = "Empty charset",
+			.str = "hello,world",
+			.charset = "",
+			.expected = (char *[]){"hello,world", NULL},
+		},
+		{
+			.desc = "String with only separators",
+			.str = ",,,,",
+			.charset = ",",
+			.expected = (char *[]){NULL},
+		},
+		{
+			.desc = "String starting and ending with separators",
+			.str = ",hello,world,",
+			.charset = ",",
+			.expected = (char *[]){"hello", "world", NULL},
+		},
+		{
+			.desc = "Multiple consecutive separators",
+			.str = "one,,two,,,three",
+			.charset = ",",
+			.expected = (char *[]){"one", "two", "three", NULL},
+		},
+		{
+			.desc = "Split with whitespace separators",
+			.str = "  hello   world  how are   you  ",
+			.charset = " ",
+			.expected = (char *[]){"hello", "world", "how", "are", "you", NULL},
+		},
+		{
+			.desc = "Split with tab and newline separators",
+			.str = "line1\tword1\nline2\tword2",
+			.charset = "\t\n",
+			.expected = (char *[]){"line1", "word1", "line2", "word2", NULL},
+		},
+		{
+			.desc = "Split with all ASCII separators",
+			.str = "a!b\"c#d$e%f&g'h(i)j*k+l,m-n.o/p:q;r<s=t>u?v@w[x]y{z}",
+			.charset = "!\"#$%&'()*+,-./:;<=>?@[]{}",
+			.expected = (char *[]){"a", "b", "c", "d", "e", "f", "g", "h", "i",
+				"j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
+				"w", "x", "y", "z", NULL},
+		},
+		{
+			.desc = "Long string with few separators",
+			.str = "This is a very long string with only a few separators to test memory allocation",
+			.charset = "x",
+			.expected = (char *[]){"This is a very long string with only a few separators to test memory allocation",
+				NULL},
+		},
+		{
+			.desc = "Unicode separators",
+			.str = "hello世界how你好are你好you",
+			.charset = "世界你好",
+			.expected = (char *[]){"hello", "how", "are", "you", NULL},
+		},
+		{
+			.desc = "Empty string with empty charset",
+			.str = "",
+			.charset = "",
+			.expected = (char *[1]){0},
+		},
+		{
+			.desc = "Single-word string with empty charset",
+			.str = "hello",
+			.charset = "",
+			.expected = (char *[2]){"hello", 0},
+		},
+		{
+			.desc = "String with leading and trailing separators",
+			.str = ",,hello,world,,",
+			.charset = ",",
+			.expected = (char *[3]){"hello", "world", 0},
+		},
+		{
+			.desc = "String with multiple consecutive separators",
+			.str = "hello,,,,world",
+			.charset = ",",
+			.expected = (char *[3]){"hello", "world", 0},
+		},
+		{
+			.desc = "String with repeated separators",
+			.str = "aaabbbaaaccc",
+			.charset = "ab",
+			.expected = (char *[]){"ccc", 0},
+		},
+	};
+	count = sizeof(tests) / sizeof(tests[0]);
+	return (run_tests(tests, count));
 }
 
-int run_tests(t_test *tests, int count)
+int	run_tests(t_test *tests, int count)
 {
-    int i;
-    int error = 0;
+	int		i;
+	int		error;
+	char	**result;
+	int		j;
 
-    for (i = 0; i < count; i++)
-    {
-        char **result = ft_split(tests[i].str, tests[i].charset);
-
-        if (!result && !tests[i].expected)
-        {
-            printf(GREEN CHECKMARK GREY " [%d] %s\n" DEFAULT, i + 1, tests[i].desc);
-        }
-        else if (!result || !tests[i].expected)
-        {
-            printf(RED "[%d] %s got \"", i + 1, tests[i].desc);
-            if (result)
-            {
-                printf("%s", result[0]);
-                for (int j = 1; result[j]; j++)
-                {
-                    printf("\", \"%s", result[j]);
-                }
-            }
-            else
-            {
-                printf("(null)");
-            }
-            printf("\" instead of \"");
-            if (tests[i].expected)
-            {
-                printf("%s", tests[i].expected[0]);
-                for (int j = 1; tests[i].expected[j]; j++)
-                {
-                    printf("\", \"%s", tests[i].expected[j]);
-                }
-            }
-            else
-            {
-                printf("(null)");
-            }
-            printf("\"\n" DEFAULT);
-            error -= 1;
-        }
-        else
-        {
-            int j = 0;
-            while (tests[i].expected[j] && result[j])
-            {
-                if (strcmp(tests[i].expected[j], result[j]) != 0)
-                {
-                    printf(RED "[%d] %s Element %d: expected \"%s\", got \"%s\"\n" DEFAULT, i + 1, tests[i].desc, j, tests[i].expected[j], result[j]);
-                    error -= 1;
-                    break;
-                }
-                j++;
-            }
-
-            if (tests[i].expected[j] != result[j])
-            {
-                printf(RED "[%d] %s got \"", i + 1, tests[i].desc);
-                if (result)
-                {
-                    printf("%s", result[0]);
-                    for (int j = 1; result[j]; j++)
-                    {
-                        printf("\", \"%s", result[j]);
-                    }
-                }
-                else
-                {
-                    printf("(null)");
-                }
-                printf("\" instead of \"");
-                if (tests[i].expected)
-                {
-                    printf("%s", tests[i].expected[0]);
-                    for (int j = 1; tests[i].expected[j]; j++)
-                    {
-                        printf("\", \"%s", tests[i].expected[j]);
-                    }
-                }
-                else
-                {
-                    printf("(null)");
-                }
-                printf("\"\n" DEFAULT);
-                error -= 1;
-            }
-            else
-            {
-                printf(GREEN CHECKMARK GREY " [%d] %s got \"", i + 1, tests[i].desc);
-                if (result)
-                {
-                    printf("%s", result[0]);
-                    for (int j = 1; result[j]; j++)
-                    {
-                        printf("\", \"%s", result[j]);
-                    }
-                }
-                printf("\" as expected\n" DEFAULT);
-            }
-        }
-
-        if (result)
-        {
-            int j = 0;
-            while (result[j])
-            {
-                free(result[j]);
-                j++;
-            }
-            free(result);
-        }
-    }
-
-    return (error);
+	error = 0;
+	for (i = 0; i < count; i++)
+	{
+		result = ft_split(tests[i].str, tests[i].charset);
+		
+		if (!result[0] && !tests[i].expected[0])
+			printf(GREEN CHECKMARK GREY " [%d] %s\n" DEFAULT, i + 1,tests[i].desc);
+		
+		else if (!result[0] || !tests[i].expected[0])
+		{
+			printf(RED "[%d] %s got \"", i + 1, tests[i].desc);
+			if (result[0])
+			{
+				printf("%s", result[0]);
+				for (int j = 1; result[j]; j++)
+				{
+					printf("\", \"%s", result[j]);
+				}
+			}
+			else
+				printf("(null)");
+			
+			printf("\" instead of \"");
+			if (tests[i].expected[0])
+			{
+				printf("%s", tests[i].expected[0]);
+				for (int j = 1; tests[i].expected[j]; j++)
+				{
+					printf("\", \"%s", tests[i].expected[j]);
+				}
+			}
+			else
+				printf("(null)");
+			
+			printf("\"\n" DEFAULT);
+			error -= 1;
+		}
+		else
+	{
+			j = 0;
+			while (tests[i].expected[j] && result[j])
+			{
+				if (strcmp(tests[i].expected[j], result[j]) != 0)
+				{
+	  printf(RED "[%d] %s Element %d: expected \"%s\",got \"%s\"\n" DEFAULT, i + 1, tests[i].desc, j,tests[i].expected[j], result[j]);
+					error -= 1;
+					break ;
+				}
+				j++;
+			}
+			if (tests[i].expected[j] != result[j])
+			{
+				printf(RED "[%d] %s got \"", i + 1, tests[i].desc);
+				if (result[0])
+				{
+					printf("%s", result[0]);
+					for (int j = 1; result[j]; j++)
+					{
+						printf("\", \"%s", result[j]);
+					}
+				}
+				else
+			{
+					printf("(null)");
+				}
+				printf("\" instead of \"");
+				if (tests[i].expected[0])
+				{
+					printf("%s", tests[i].expected[0]);
+					for (int j = 1; tests[i].expected[j]; j++)
+					{
+						printf("\", \"%s", tests[i].expected[j]);
+					}
+				}
+				else
+			{
+					printf("(null)");
+				}
+				printf("\"\n" DEFAULT);
+				error -= 1;
+			}
+			else
+		{
+	  printf(GREEN CHECKMARK GREY " [%d] %s got \"", i + 1,tests[i].desc);
+				if (result[0])
+				{
+					printf("%s", result[0]);
+					for (int j = 1; result[j]; j++)
+					{
+						printf("\", \"%s", result[j]);
+					}
+				}
+				printf("\" as expected\n" DEFAULT);
+			}
+		}
+		if (result[0])
+		{
+			j = 0;
+			while (result[j])
+			{
+				free(result[j]);
+				j++;
+			}
+			free(result);
+		}
+	}
+	return (error);
 }
